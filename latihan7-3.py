645e14997d47958f25c5a2b22a9fced9c111085a
gabrielsachioa <143605434+gabrielsachioa@users.noreply.github.com>
2024-03-25 15:22:38 +0700

    Delete latihan7-3.py

--- latihan7-3.py
+++
@@ -1,8 +0,0 @@
-def hapus_kelebihan_spasi(kalimat):
-    kalimat = kalimat.lower()
-    kalimat = " ".join(kalimat.split())
-    print(kalimat)
-
-# PROGRAM UTAMA
-# kalimat = input("Kalimat: ")
-hapus_kelebihan_spasi("saya tidak  suka memancing ikan ")
